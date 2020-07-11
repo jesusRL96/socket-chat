dd8911e7218523b3601bc1fbcc1dac613791b741
jesus <jesusalbertoramirezlopez.jr@gmail.com>
2020-07-10 20:59:47 -0500

    Chat finalizado V1

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -17,13 +17,15 @@ io.on('connect', (client) => {
     client.join(usuario.sala)
     let personas = usuarios.agregarPersona(client.id, usuario.nombre, usuario.sala);
     client.broadcast.to(usuario.sala).emit('listaPersonas', usuarios.getPersonasPorSala(usuario.sala));
+    client.broadcast.to(usuario.sala).emit('crearMensaje',crearMensaje('Administrador', `${usuario.nombre} se unio`));
     callback(usuarios.getPersonasPorSala(usuario.sala));
   });
 
-  client.on('crearMensaje', (data) => {
+  client.on('crearMensaje', (data, callback) => {
     let persona = usuarios.getPersona(client.id);
     let mensaje = crearMensaje(persona.nombre, data.mensaje);
     client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+    callback(mensaje);
   });
 
   client.on('disconnect', () => {
